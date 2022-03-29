bfc9d4e772838a69c43e83ae95042de161a4795e
David Coles <davidepcoles@gmail.com>
2022-03-29 11:21:42 -0700

    added button and panels

--- home.Designer.cs
+++ home.Designer.cs
@@ -29,26 +29,63 @@
         private void InitializeComponent()
         {
             this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelTopMenu = new System.Windows.Forms.Panel();
+            this.panelSideMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelSideMenu
             // 
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panelSideMenu.Controls.Add(this.button1);
+            this.panelSideMenu.Controls.Add(this.panelLogo);
             this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
             this.panelSideMenu.Name = "panelSideMenu";
             this.panelSideMenu.Size = new System.Drawing.Size(250, 970);
             this.panelSideMenu.TabIndex = 0;
             // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(250, 100);
+            this.panelLogo.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panelTopMenu
+            // 
+            this.panelTopMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelTopMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopMenu.Location = new System.Drawing.Point(250, 0);
+            this.panelTopMenu.Name = "panelTopMenu";
+            this.panelTopMenu.Size = new System.Drawing.Size(1569, 100);
+            this.panelTopMenu.TabIndex = 1;
+            // 
             // home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1819, 970);
+            this.Controls.Add(this.panelTopMenu);
             this.Controls.Add(this.panelSideMenu);
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "home";
             this.Text = "home";
             this.Load += new System.EventHandler(this.home_Load);
+            this.panelSideMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +93,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTopMenu;
     }
 }
